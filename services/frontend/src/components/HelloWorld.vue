c9b0409f180a229b79cc7e1f65c20d37d8e55148
Miguel Acevedo <44342378+miguelalb@users.noreply.github.com>
2022-02-06 20:56:42 -0400

    Frontend init - forgot .dockerignore

---
+++ services/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    {{info }}
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      info: null
+    }
+  },
+  mounted() {
+    axios
+    .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then((res) => {
+      this.info = res;
+    }).catch(err => console.log(err));
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
